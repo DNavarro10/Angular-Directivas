80b4c1034d6ffe78ce45b73b80105c47b3efca48
DNavarro10 <luis_d_100@hotmail.com>
2019-07-04 00:16:33 -0600

    Card - Faltan estilos

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomersCardComponent } from './customers-card/customers-card.component';
-import { MaterialModule } from '../Material/materia';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatCardModule, MatIconModule } from '@angular/material';
+
 
 
 @NgModule({
-  declarations: [CustomerListComponent, CustomersCardComponent],
+  declarations: [CustomerListComponent, CustomersCardComponent,],
   imports: [
     CommonModule,
     CustomersRoutingModule,
-    
-    MaterialModule
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class CustomersModule { }
