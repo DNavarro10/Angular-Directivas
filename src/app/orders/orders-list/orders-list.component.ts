972d27c8e125a521e8978a12ea5a89e652085cb7
DNavarro10 <luis_d_100@hotmail.com>
2019-07-15 15:51:23 -0600

    length sacado, sort dejo de servir

--- src/app/orders/orders-list/orders-list.component.ts
+++ src/app/orders/orders-list/orders-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort, MatTableDataSource, PageEvent} from '@angular/material';
+import { MatSort, MatTableDataSource, PageEvent } from '@angular/material';
 import { ServiciosService } from '../../services/servicios.service';  //importar servicios
 
 
@@ -12,55 +12,59 @@ import { ServiciosService } from '../../services/servicios.service';  //importar
   selector: 'app-orders-list',
   templateUrl: './orders-list.component.html',
   styleUrls: ['./orders-list.component.sass'],
-  providers:[ServiciosService]  // necesario para llamar servicios
+  providers: [ServiciosService]  // necesario para llamar servicios
 })
 export class OrdersListComponent implements OnInit {
- 
- //datos desde serviciios
- public ELEMENT_DATA = []; 
- 
+
+  //datos desde serviciios
+  public ELEMENT_DATA = [];
+
   //columnas y datos
   displayedColumns: string[] = ['action', 'orderNumber', 'orderDate', 'description', 'total'];
-  dataSource:MatTableDataSource<object>; //cargar datos - los nuevos seran con el loadData
-   
-  
+  dataSource: MatTableDataSource<object>; //cargar datos - los nuevos seran con el loadData
+  largo: any = 0;
   //propiedades del paginator
-    length:Array<number> = this.ELEMENT_DATA;  //this. pora referir al public
-    pageIndex:number = 0;
-    pageSize:number = 10;
-    pageSizeOptions:Array<number> = [1,2,5,10];
-    
+  length: number = 0 ;  //this. pora referir al public
+  pageIndex: number = 0;
+  pageSize: number = 10;
+  pageSizeOptions: Array<number> = [1, 2, 5, 10];
 
-    pageEvent:PageEvent; //objeto event
+
+  pageEvent: PageEvent; //objeto event
 
   //observer para el sort
-  @ViewChild( MatSort, {static: false}) sort: MatSort;
-  
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+
   //se declara servicio usando  _Nombre + servicio
-  constructor( private _servicio: ServiciosService) { }
+  constructor(private _servicio: ServiciosService) {
+    // Get the size of an object
+  }
 
   ngOnInit() {
     //se debe iniciar los datos en el init
+    this.largo = this._servicio.array();
     this.ELEMENT_DATA = this._servicio.getDatos();
-    
-    this.loadData(0, this.pageSize);this.dataSource.sort = this.sort;
+    this.length =  Object.size(this.ELEMENT_DATA)
 
-    console.log(typeof this.ELEMENT_DATA);
+
+    this.loadData(0, this.pageSize); this.dataSource.sort = this.sort;
+
+
+
+
   }
 
   //funcion para el checkbox, selecionar todos
-  selectAll(){
-    for (var element of this.ELEMENT_DATA) 
-    {
+  selectAll() {
+    for (var element of this.ELEMENT_DATA) {
       element.isChecked = !element.isChecked
     }
   }
   //funcion para cargar el page desde x punto
-  loadData(pageIndex,pageSize)  
-  {
+  loadData(pageIndex, pageSize) {
     this.dataSource = new MatTableDataSource<Object>(this.ELEMENT_DATA.slice(pageIndex, pageIndex + pageSize)); // p + pS para ver el incremento del array
   }
-  onPageChange(e){
+  onPageChange(e) {
 
     this.pageIndex = e.pageIndex * this.pageSize;
 
