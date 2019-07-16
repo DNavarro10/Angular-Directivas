cfce474aa703fe3fcc832b3eef9f223b57645279
DNavarro10 <luis_d_100@hotmail.com>
2019-07-15 18:53:03 -0600

    arreglado

--- src/app/orders/orders-list/orders-list.component.ts
+++ src/app/orders/orders-list/orders-list.component.ts
@@ -33,7 +33,7 @@ export class OrdersListComponent implements OnInit {
   pageEvent: PageEvent; //objeto event
 
   //observer para el sort
-  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   //se declara servicio usando  _Nombre + servicio
   constructor(private _servicio: ServiciosService) {
@@ -42,17 +42,15 @@ export class OrdersListComponent implements OnInit {
 
   ngOnInit() {
     //se debe iniciar los datos en el init
-    this.largo = this._servicio.array();
+    //operacion para sacar lenght del array en el servicio, largo recibe & length muestra
+    this.largo = this._servicio.array(); 
     this.ELEMENT_DATA = this._servicio.getDatos();
-    this.length =  Object.size(this.ELEMENT_DATA)
-
-
+    this.length =  Object.size(this.ELEMENT_DATA);
+    //carga la informacion al iniciar
     this.loadData(0, this.pageSize); this.dataSource.sort = this.sort;
 
-
-
-
   }
+  
 
   //funcion para el checkbox, selecionar todos
   selectAll() {
@@ -61,16 +59,20 @@ export class OrdersListComponent implements OnInit {
     }
   }
   //funcion para cargar el page desde x punto
-  loadData(pageIndex, pageSize) {
+  loadData(pageIndex: number, pageSize: number) {
     this.dataSource = new MatTableDataSource<Object>(this.ELEMENT_DATA.slice(pageIndex, pageIndex + pageSize)); // p + pS para ver el incremento del array
   }
-  onPageChange(e) {
 
-    this.pageIndex = e.pageIndex * this.pageSize;
+  onPageChange(e) {
+    //calcula los valores del paginador
+    this.pageIndex = e.pageIndex * this.pageSize; //calcula el index
 
     this.pageSize = e.pageSize;
 
     this.loadData(this.pageIndex, this.pageSize);
+
+    //reinicia los valores del sort dependiendo de la pag en que se encuentra
+    this.loadData(0, this.pageSize); this.dataSource.sort = this.sort;
 
   }
 }
